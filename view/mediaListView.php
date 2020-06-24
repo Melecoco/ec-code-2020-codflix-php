4509294803c14cdcc9f513ac1dde619e3b7107b7
Unknown <duhalgouetm@yahoo.com>
2020-06-24 12:12:42 +0200

    Api call in MediaListView working

--- view/mediaListView.php
+++ view/mediaListView.php
@@ -1,5 +1,30 @@
 <?php ob_start(); ?>
 
+<?php
+function getMediaFromApi(){
+
+$api_key = "b3614915c9940749621d53925f4408c9";
+$api_url_example = "https://api.themoviedb.org/3/movie/550?api_key=";
+$api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzYxNDkxNWM5OTQwNzQ5NjIxZDUzOTI1ZjQ0MDhjOSIsInN1YiI6IjVkMGI1OTc1YzNhMzY4NjJmYTFlNTJlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jfYiZG-ydiK4hZuMWYgpUPTtp_dOnfWOuJz4WCbQo0E";
+$xml = file_get_contents("http://www.example.com/file.xml");
+
+  $headers = array(
+    'Content-Type: application/json',
+    sprintf('Authorization: Bearer %s', $api_token)
+  );
+
+  $curl = curl_init($api_url_example . $api_key);
+
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+  $result = json_decode(curl_exec($curl));
+
+  var_dump($result);
+}
+getMediaFromApi();
+?>
+
 <div class="row">
     <div class="col-md-4 offset-md-8">
         <form method="get">
